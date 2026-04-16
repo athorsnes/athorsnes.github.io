0a91a3e1f9a0d2cc9a5c0b336975c7c5290b2712
athorsnes <antonth91@gmail.com>
2026-04-16 19:59:23 +0200

    Replace Flutter app with Trellegant Power-Up at root

--- trellegant/js/config.js
+++
@@ -1,5 +0,0 @@
-// Trellegant Power-Up Configuration
-// Replace API_KEY with your key from https://trello.com/app-key
-const TRELLEGANT_CONFIG = {
-  API_KEY: 'de386cfcdf4a932ef22ac95a5e2fe421',
-};
